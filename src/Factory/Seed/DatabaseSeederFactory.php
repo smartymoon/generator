a0ba5d9df63cfbd920201449a1112231b6e3170a
厉增伟 <lee@izhuoyi.com>
2020-11-17 14:09:38 +0800

    update

--- src/Factory/Seed/DatabaseSeederFactory.php
+++ src/Factory/Seed/DatabaseSeederFactory.php
@@ -17,7 +17,7 @@ class DatabaseSeederFactory extends MakeFactory implements FactoryContract
 
     public function buildContent(string $content): string
     {
-        return str_replace('//DummySeeder', $this->dealModuleInUse() . $this->injectSeeder(), $content);
+        return str_replace('//DummySeeder',  $this->injectSeeder(), $content);
     }
 
     public function getFilePath(): string
@@ -27,7 +27,7 @@ class DatabaseSeederFactory extends MakeFactory implements FactoryContract
 
     private function injectSeeder(): string
     {
-        return '$this->call('.$this->getModelClass().'Seeder::class);'."\n".
+        return '$this->call('. $this->dealModuleInUse() . $this->getModelClass().'Seeder::class);'."\n".
                $this->tab(2).'//DummySeeder';
     }
 
